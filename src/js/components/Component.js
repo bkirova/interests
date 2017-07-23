11e3af5a256d7dbb938a55bf8c88f3f0ace1620a
itsachev <itsa4ev@gmail.com>
2017-07-23 20:14:22 +0300

    Add component methods

--- src/js/components/Component.js
+++ src/js/components/Component.js
@@ -6,7 +6,71 @@ class Component extends EventEmitter {
 	constructor(name, data) {
 		super();
 
-	
+        /*
+        * Check if user exist
+        */
+        _getUser() {
+
+        }
+
+        /*
+        * Login User
+        */
+        _logIn() {
+
+        }
+
+        /*
+        * Register User
+        */
+        _register() {
+
+        }
+
+        /*
+        * Get user data
+        */
+        _getUserData() {
+
+        }
+
+        /*
+        * Get user interests
+        */
+        _getInteresets() {
+
+        }
+
+        /*
+        * Create new interest
+        */
+        _setInterest() {
+
+        }
+
+        /*
+        * Get All latest interesets that match users inteterests
+        */
+        _getLatestInterest() {
+
+        }
+
+        /*
+        * Update state
+        */
+        _setState() {
+
+        }
+
+        /*
+        * Render User
+        */
+        _render() {
+
+        }
+
+
+
 	}
 
 
